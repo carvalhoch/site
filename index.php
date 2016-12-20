c99d888069fb4f690dfea298eb878163c276f580
Carlos Henrique <carvalho.ch@gmail.com>
2016-12-20 15:43:47 -0200

    Alteração no index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-	echo 'TESTE';
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>TESTE</title>
+<meta name="description" content="TESTE">
+</head>
+<body>
+
+</body>
+</body>
